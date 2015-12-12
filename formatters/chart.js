6a3462a99be3b036cb35de45ea523995131d2b3e
Eero Helenius <eero.helenius@citec.com>
2015-12-12 09:13:36 +0200

    Calculate chart size based on terminal size

--- formatters/chart.js
+++ formatters/chart.js
@@ -1,12 +1,15 @@
 'use strict'
 
-const
+const process = require('process'),
   chart = require('chart'),
-  R     = require('ramda')
-
-const options = { width: 80, height: 24 }
+  R = require('ramda')
 
 module.exports = (data) => {
+  let options = {
+    width: Math.floor(process.stdout.columns / 2),
+    height: process.stdout.rows
+  }
+
   let formatter = R.flip(R.curry(chart))(options)
   return R.join(' ', R.map(R.pipe(R.tail, formatter), data))
 }
